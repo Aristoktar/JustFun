970b2146d1524315fa626b786313f060ab64d103
Aristoktar <alexey0206@gmail.com>
2015-09-07 18:04:17 +0400

    CharacterCapacitiesParse

--- ParserForQRCode/ParserForQRCode/Form1.Designer.cs
+++ ParserForQRCode/ParserForQRCode/Form1.Designer.cs
@@ -23,29 +23,32 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent () {
-			this.textBoxInput = new System.Windows.Forms.TextBox();
-			this.labelInput = new System.Windows.Forms.Label();
+			this.textBoxInputUrl = new System.Windows.Forms.TextBox();
+			this.labelInputUrl = new System.Windows.Forms.Label();
 			this.labelOutput = new System.Windows.Forms.Label();
 			this.textBoxOutput = new System.Windows.Forms.TextBox();
 			this.buttonParse = new System.Windows.Forms.Button();
+			this.textBoxInputTableId = new System.Windows.Forms.TextBox();
+			this.labelDivId = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
-			// textBoxInput
+			// textBoxInputUrl
 			// 
-			this.textBoxInput.Location = new System.Drawing.Point(12, 47);
-			this.textBoxInput.Multiline = true;
-			this.textBoxInput.Name = "textBoxInput";
-			this.textBoxInput.Size = new System.Drawing.Size(258, 219);
-			this.textBoxInput.TabIndex = 0;
+			this.textBoxInputUrl.Location = new System.Drawing.Point(12, 47);
+			this.textBoxInputUrl.Name = "textBoxInputUrl";
+			this.textBoxInputUrl.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBoxInputUrl.Size = new System.Drawing.Size(258, 20);
+			this.textBoxInputUrl.TabIndex = 0;
+			this.textBoxInputUrl.Text = "http://www.thonky.com/qr-code-tutorial/character-capacities";
 			// 
-			// labelInput
+			// labelInputUrl
 			// 
-			this.labelInput.AutoSize = true;
-			this.labelInput.Location = new System.Drawing.Point(77, 18);
-			this.labelInput.Name = "labelInput";
-			this.labelInput.Size = new System.Drawing.Size(31, 13);
-			this.labelInput.TabIndex = 1;
-			this.labelInput.Text = "Input";
+			this.labelInputUrl.AutoSize = true;
+			this.labelInputUrl.Location = new System.Drawing.Point(77, 18);
+			this.labelInputUrl.Name = "labelInputUrl";
+			this.labelInputUrl.Size = new System.Drawing.Size(44, 13);
+			this.labelInputUrl.TabIndex = 1;
+			this.labelInputUrl.Text = "InputUrl";
 			// 
 			// labelOutput
 			// 
@@ -61,6 +64,7 @@
 			this.textBoxOutput.Location = new System.Drawing.Point(453, 47);
 			this.textBoxOutput.Multiline = true;
 			this.textBoxOutput.Name = "textBoxOutput";
+			this.textBoxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
 			this.textBoxOutput.Size = new System.Drawing.Size(295, 219);
 			this.textBoxOutput.TabIndex = 3;
 			// 
@@ -72,17 +76,37 @@
 			this.buttonParse.TabIndex = 4;
 			this.buttonParse.Text = "->";
 			this.buttonParse.UseVisualStyleBackColor = true;
+			this.buttonParse.Click += new System.EventHandler(this.buttonParse_Click);
+			// 
+			// textBoxInputTableId
+			// 
+			this.textBoxInputTableId.Location = new System.Drawing.Point(32, 117);
+			this.textBoxInputTableId.Name = "textBoxInputTableId";
+			this.textBoxInputTableId.Size = new System.Drawing.Size(100, 20);
+			this.textBoxInputTableId.TabIndex = 5;
+			this.textBoxInputTableId.Text = "body-text";
+			// 
+			// labelDivId
+			// 
+			this.labelDivId.AutoSize = true;
+			this.labelDivId.Location = new System.Drawing.Point(40, 99);
+			this.labelDivId.Name = "labelDivId";
+			this.labelDivId.Size = new System.Drawing.Size(32, 13);
+			this.labelDivId.TabIndex = 6;
+			this.labelDivId.Text = "DivId";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(1033, 359);
+			this.Controls.Add(this.labelDivId);
+			this.Controls.Add(this.textBoxInputTableId);
 			this.Controls.Add(this.buttonParse);
 			this.Controls.Add(this.textBoxOutput);
 			this.Controls.Add(this.labelOutput);
-			this.Controls.Add(this.labelInput);
-			this.Controls.Add(this.textBoxInput);
+			this.Controls.Add(this.labelInputUrl);
+			this.Controls.Add(this.textBoxInputUrl);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.ResumeLayout(false);
@@ -92,11 +116,13 @@
 
 		#endregion
 
-		private System.Windows.Forms.TextBox textBoxInput;
-		private System.Windows.Forms.Label labelInput;
+		private System.Windows.Forms.TextBox textBoxInputUrl;
+		private System.Windows.Forms.Label labelInputUrl;
 		private System.Windows.Forms.Label labelOutput;
 		private System.Windows.Forms.TextBox textBoxOutput;
 		private System.Windows.Forms.Button buttonParse;
+		private System.Windows.Forms.TextBox textBoxInputTableId;
+		private System.Windows.Forms.Label labelDivId;
 	}
 }
 
--- ParserForQRCode/ParserForQRCode/Form1.cs
+++ ParserForQRCode/ParserForQRCode/Form1.cs
@@ -1,17 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace ParserForQRCode {
 	public partial class Form1 : Form {
 		public Form1 () {
 			InitializeComponent ();
+		}
+
+		private void buttonParse_Click ( object sender , EventArgs e )
+		{
+			var req = WebRequest.Create(this.textBoxInputUrl.Text);
+			string html = new StreamReader(req.GetResponse().GetResponseStream()).ReadToEnd();
+
+			HtmlDocument htmlDocument;
+			XmlDocument  doc = new XmlDocument();
+
+			doc.LoadXml(html.Replace("&","").Replace("\n","").Replace("\r",""));
+			var table = doc.GetElementsByTagName("table").Item(0);
+
+			table.RemoveChild(table.FirstChild);
+
+			var nodes =table.ChildNodes;
+
+			string lastVersion = "-1";
+			int rowCount = 0;
+
+			XmlDocument document = new XmlDocument();
+
+			StringBuilder str = new StringBuilder();
+
+			foreach (XmlNode nodeTr in nodes)
+			{
+				str.Append( "{ ");
+				if (nodeTr.FirstChild.Attributes["rowspan"]!=null)
+				{
+					lastVersion = nodeTr.FirstChild.FirstChild.Value;
+				}
+				else
+				{
+					str.Append ( lastVersion + ", " );
+				}
+				for (int i = 0; i < nodeTr.ChildNodes.Count-1; i++)
+				{
+					str.Append( nodeTr.ChildNodes[i].FirstChild.Value+", ");
+				}
+
+				str.Append( nodeTr.ChildNodes[nodeTr.ChildNodes.Count - 1].FirstChild.Value + " },");
+				str.Append( System.Environment.NewLine);
+			}
+
+			this.textBoxOutput.Text = str.ToString ().Replace ( "L" , "0" ).Replace ( "M" , "1" ).Replace ( "Q" , "2" ).Replace ( "H" , "3" );
+
 		}
 	}
 }
